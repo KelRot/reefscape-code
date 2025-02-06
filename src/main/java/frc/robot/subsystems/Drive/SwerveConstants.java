1bdae463f11343980ae3111912d2e77f3dd06320
recepsirin0 <recepsirin1@outlook.com>
2025-02-06 21:32:06 +0300

    Swerve All Code

---
+++ src/main/java/frc/robot/subsystems/Drive/SwerveConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.Drive;
+
+
+public class SwerveConstants {
+    public final static double MAX_SPEED = 3;
+    public final static double WHEEL_LOCK_TIME = 10; // seconds
+}
