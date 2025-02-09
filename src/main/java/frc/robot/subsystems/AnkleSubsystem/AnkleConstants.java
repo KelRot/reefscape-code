89c578fb66518c3f726afdb926c3b04d0beaf67b
recepsirin0 <recepsirin1@outlook.com>
2025-02-09 20:09:29 +0300

    Added Ankle Subsystem - It includes basic level functions ( setAngle / setAngleTest / stopMotors )

---
+++ src/main/java/frc/robot/subsystems/AnkleSubsystem/AnkleConstants.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.AnkleSubsystem;
+
+public class AnkleConstants {
+    public final static int RedlineID = 1;
+    public final static int SparkID = 1;
+    public final static double maxAngle = 0;
+    public final static double minAngle = 0;
+    public static double kP = 0.04;
+    public static double kI = 0;
+    public static double kD = 0;
+
+}
---
+++ src/main/java/frc/robot/subsystems/AnkleSubsystem/AnkleSubsystem.java
@@ -0,0 +1,91 @@
+package frc.robot.subsystems.AnkleSubsystem;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AnkleSubsystem extends SubsystemBase {
+  private SparkMax m_motor;
+  private WPI_VictorSPX m_wheelMotor;
+  private SparkClosedLoopController closedLoopController;
+  private SparkMaxConfig motorConfig;
+  private RelativeEncoder encoder;
+
+  public AnkleSubsystem() {
+    m_motor = new SparkMax(AnkleConstants.SparkID, MotorType.kBrushless);
+    m_wheelMotor = new WPI_VictorSPX(AnkleConstants.RedlineID);
+    encoder = m_motor.getEncoder();
+    closedLoopController = m_motor.getClosedLoopController();
+    motorConfig = new SparkMaxConfig();
+ 
+    motorConfig.closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        .p(AnkleConstants.kP)
+        .i(AnkleConstants.kI)
+        .d(AnkleConstants.kD)
+        .outputRange(-0.9, 0.9);
+    
+    motorConfig.closedLoop.maxMotion
+        .maxVelocity(1000)
+        .maxAcceleration(1000)
+        .allowedClosedLoopError(1);
+    m_motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+  }
+
+  public void setAngle(double angle) {
+    if(isAngleInRange(angle)) {
+    closedLoopController.setReference(angle, ControlType.kMAXMotionPositionControl,
+          ClosedLoopSlot.kSlot0); 
+        } else {
+          setDefault();
+        }
+  }
+  public void setAngleTest() {
+    double angle = SmartDashboard.getNumber("testAngle Ankle", 0);
+    if(isAngleInRange(angle)) {
+    closedLoopController.setReference(angle, ControlType.kMAXMotionPositionControl,
+          ClosedLoopSlot.kSlot0); 
+        } else {
+          setDefault();
+        }
+  }
+  public void setDefault() {
+    closedLoopController.setReference(0, ControlType.kMAXMotionPositionControl,
+          ClosedLoopSlot.kSlot0);
+    if(encoder.getPosition() == 0) {
+    stopAngleMotor();
+    }
+  }
+  public boolean isAngleInRange(double angle) {
+    return angle <= AnkleConstants.maxAngle && angle >= AnkleConstants.minAngle;
+  }
+  public void stopAngleMotor() {
+    m_motor.set(0);
+  }
+  public void setWheelMotor(double velocity) {
+    m_wheelMotor.set(velocity);
+  }
+  @Override
+  public void periodic() {
+    encoder.setPosition(0);
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
